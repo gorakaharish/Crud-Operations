bb61844c593c37c37873354a452df786a6cd9f0f
G Harish <147321088+harishgoraka@users.noreply.github.com>
2024-02-09 14:50:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,27 @@
+import axiso from "axios";
+
+const URL = "http://localhost:26172/products";
+export async function getData() {
+  return await axiso.get(URL);
+}
+
+export async function deleteData(id) {
+  return await axiso.delete(`${URL}/${id}`);
+}
+
+export async function postData(data) {
+  return await axiso.post(URL, data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function putData(id, data) {
+  return await axiso.put(URL + "/" + id, data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
 
